2f902b3cf0dd717cb959e48c02fb04e03898213b
Edelmir <35733@mail.ru>
2016-08-01 21:54:14 +0400

    Add files via upload

---
+++ Bedroom.java
@@ -0,0 +1,8 @@
+/**
+ * Created by edelm on 01.08.2016.
+ */
+public class Bedroom {
+    public static void main(String[] args) {
+        System.out.println("We are in bedroom");
+    }
+}
--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 /**
  * Created by edelm on 01.08.2016.
  */
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("main");
+        cookSoup();
+    }
+    static void cookSoup() {
+        System.out.println("Hello");
+    }
 }
